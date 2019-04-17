5fff9ddd78e273ef04686348b80ead93ab555fc0
Ross <Theadora.Ross@tyson.com>
2019-04-17 11:20:43 -0500

    testing

--- mdata_payload/mdata_payload_test.go
+++ mdata_payload/mdata_payload_test.go
@@ -10,9 +10,9 @@ var testPayloads = []struct {
 	outPayload *MdPayload
 	outError   error
 }{
-	{nil, nil, processor.InvalidTransactionError{Msg: "Must contain payload"}},
+	{nil, nil, &processor.InvalidTransactionError{Msg: "Must contain payload"}},
 	//{[]byte("create"), nil, &processor.InvalidTransactionError{Msg: "Payload is malformed"}}, //len<2
-	//{[]byte("create,00012345600012,000000001400245446"), &MdPayload{Action: "create", Gtin: "00012345600012", Mtrl: "000000001400245446"}, nil},
+	{[]byte("create,00012345600012,000000001400245446"), &MdPayload{Action: "create", Gtin: "00012345600012", Mtrl: "000000001400245446"}, nil},
 	//{[]byte("create,00012345600012"), nil, &processor.InvalidTransactionError{Msg: "Mtrl is required for create and update"}},
 	//{[]byte("update,00012345600012,000000001400245446"), &MdPayload{Action: "update", Gtin: "00012345600012", Mtrl: "000000001400245446"}, nil},
 	//{[]byte("update,00012345600012"), nil, &processor.InvalidTransactionError{Msg: "Mtrl is required for create and update"}},
@@ -21,7 +21,7 @@ var testPayloads = []struct {
 func TestFromBytes(t *testing.T) {
 	for _, tt := range testPayloads {
 		payload, err := FromBytes(tt.in)
-		if payload != tt.outPayload || &err != &tt.outError {
+		if payload != tt.outPayload || err != tt.outError {
 			t.Errorf("FromBytes(%v) => GOT %v, %v, WANT %v, %v", tt.in, payload, err, tt.outPayload, tt.outError)
 		}
 	}
