d27a208f416609dfd01ba8d2cac395e1eb2d1f7d
Ross <Theadora.Ross@tyson.com>
2019-04-18 09:54:07 -0500

    testing

--- mdata_payload/mdata_payload_test.go
+++ mdata_payload/mdata_payload_test.go
@@ -13,7 +13,7 @@ var testPayloads = []struct {
 }{
 	{nil, nil, &processor.InvalidTransactionError{Msg: "Must contain payload"}},
 	{[]byte("create"), nil, &processor.InvalidTransactionError{Msg: "Payload is malformed"}}, //len<2
-	{[]byte("create,00012345600012,000000001400245446"), *MdPayload{Action: "create", Gtin: "00012345600012", Mtrl: "000000001400245446"}, nil},
+	{[]byte("create,00012345600012,000000001400245446"), &MdPayload{Action: "create", Gtin: "00012345600012", Mtrl: "000000001400245446"}, nil},
 	//{[]byte("create,00012345600012"), nil, &processor.InvalidTransactionError{Msg: "Mtrl is required for create and update"}},
 	//{[]byte("update,00012345600012,000000001400245446"), MdPayload{Action: "update", Gtin: "00012345600012", Mtrl: "000000001400245446"}, nil},
 	//{[]byte("update,00012345600012"), nil, &processor.InvalidTransactionError{Msg: "Mtrl is required for create and update"}},
@@ -33,7 +33,7 @@ func compareExpectedActualError(expectedErr error, actualError error) bool {
 // 	if
 // }
 
-func compareExpectedActualPayload(expectedPayload MdPayload, actualPayload *MdPayload) bool {
+func compareExpectedActualPayload(expectedPayload *MdPayload, actualPayload *MdPayload) bool {
 	var areEqual bool
 	if expectedPayload != nil {
 		areEqual = &expectedPayload == actualPayload
