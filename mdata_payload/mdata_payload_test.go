0c9a783ef9ed66abe84df4f80dfc06f1f54b8e39
Ross <Theadora.Ross@tyson.com>
2019-04-18 10:42:03 -0500

    testing

--- mdata_payload/mdata_payload_test.go
+++ mdata_payload/mdata_payload_test.go
@@ -15,8 +15,8 @@ var testPayloads = []struct {
 	{[]byte("create"), nil, &processor.InvalidTransactionError{Msg: "Payload is malformed"}}, //len<2
 	{[]byte("create,00012345600012,000000001400245446"), &MdPayload{Action: "create", Gtin: "00012345600012", Mtrl: "000000001400245446"}, nil},
 	{[]byte("create,00012345600012"), nil, &processor.InvalidTransactionError{Msg: "Mtrl is required for create and update"}},
-	{[]byte("update,00012345600012,000000001400245446"), &MdPayload{Action: "update", Gtin: "00012345600012", Mtrl: "000000001400245446"}, nil},
-	{[]byte("update,00012345600012"), nil, &processor.InvalidTransactionError{Msg: "Mtrl is required for create and update"}},
+	//{[]byte("update,00012345600012,000000001400245446"), &MdPayload{Action: "update", Gtin: "00012345600012", Mtrl: "000000001400245446"}, nil},
+	//{[]byte("update,00012345600012"), nil, &processor.InvalidTransactionError{Msg: "Mtrl is required for create and update"}},
 }
 
 func compareExpectedActualError(expectedErr error, actualError error) bool {
